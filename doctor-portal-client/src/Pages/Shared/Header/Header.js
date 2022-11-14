b96839a190c4275b0d81bc4352797e7fc78d4c92
Ruhul Amin Parvez <ruhul.parvez@oisbd.net>
2022-11-14 18:41:46 +0600

    Header Links Added

--- doctor-portal-client/src/Pages/Shared/Header/Header.js
+++ doctor-portal-client/src/Pages/Shared/Header/Header.js
@@ -30,32 +30,19 @@ const Header = () => {
               className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
             >
               <li>
-                <Link>Item 1</Link>
-              </li>
-              <li tabIndex={0}>
-                <Link className="justify-between">
-                  Parent
-                  <svg
-                    className="fill-current"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
-                  </svg>
-                </Link>
-                <ul className="p-2">
-                  <li>
-                    <Link>Submenu 1</Link>
-                  </li>
-                  <li>
-                    <Link>Submenu 2</Link>
-                  </li>
-                </ul>
+                <Link>Home</Link>
               </li>
               <li>
-                <Link>Item 3</Link>
+                <Link>About</Link>
+              </li>
+              <li>
+                <Link>Appointment</Link>
+              </li>
+              <li>
+                <Link>Review</Link>
+              </li>
+              <li>
+                <Link>Contact Us</Link>
               </li>
             </ul>
           </div>
@@ -67,32 +54,19 @@ const Header = () => {
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal p-0">
             <li>
-              <Link>Item 1</Link>
-            </li>
-            <li tabIndex={0}>
-              <Link>
-                Parent
-                <svg
-                  className="fill-current"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="20"
-                  height="20"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
-                </svg>
-              </Link>
-              <ul className="p-2">
-                <li>
-                  <Link>Submenu 1</Link>
-                </li>
-                <li>
-                  <Link>Submenu 2</Link>
-                </li>
-              </ul>
+              <Link>Home</Link>
             </li>
             <li>
-              <Link>Item 3</Link>
+              <Link>About</Link>
+            </li>
+            <li>
+              <Link>Appointment</Link>
+            </li>
+            <li>
+              <Link>Review</Link>
+            </li>
+            <li>
+              <Link>Contact Us</Link>
             </li>
           </ul>
         </div>
