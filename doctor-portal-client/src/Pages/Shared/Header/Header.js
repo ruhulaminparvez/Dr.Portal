40a9d74290779d30e647e6213152cdd003b32f15
Ruhul Amin Parvez <ruhul.parvez@oisbd.net>
2022-11-14 18:18:25 +0600

    Header and Footer Design Done

--- doctor-portal-client/src/Pages/Shared/Header/Header.js
+++ doctor-portal-client/src/Pages/Shared/Header/Header.js
@@ -1,11 +1,107 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+import logo  from "../../../Assets/logos/logo-2.png";
+// import logo2  from "../../../Assets/logos/logo-2.png";
 
 const Header = () => {
-    return (
-        <div>
-            <h1>This is Header</h1>
+  return (
+    <div>
+      <div className="navbar bg-base-100 px-16" data-theme="corporate">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <Link>Item 1</Link>
+              </li>
+              <li tabIndex={0}>
+                <Link className="justify-between">
+                  Parent
+                  <svg
+                    className="fill-current"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
+                  </svg>
+                </Link>
+                <ul className="p-2">
+                  <li>
+                    <Link>Submenu 1</Link>
+                  </li>
+                  <li>
+                    <Link>Submenu 2</Link>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <Link>Item 3</Link>
+              </li>
+            </ul>
+          </div>
+          <Link className="normal-case text-xl flex justify-center items-center">
+            <img className="w-12" src={logo} alt="" />
+            Dr. Portal
+            </Link>
         </div>
-    );
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal p-0">
+            <li>
+              <Link>Item 1</Link>
+            </li>
+            <li tabIndex={0}>
+              <Link>
+                Parent
+                <svg
+                  className="fill-current"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="20"
+                  height="20"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
+                </svg>
+              </Link>
+              <ul className="p-2">
+                <li>
+                  <Link>Submenu 1</Link>
+                </li>
+                <li>
+                  <Link>Submenu 2</Link>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <Link>Item 3</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="navbar-end">
+          <Link className="btn">LogIn</Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
