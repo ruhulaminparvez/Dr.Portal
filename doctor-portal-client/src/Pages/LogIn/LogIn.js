4695b6ddcb359369bfdd6f0cf31a2444f07d026b
Ruhul Amin Parvez <ruhul.parvez@oisbd.net>
2022-11-16 14:29:38 +0600

    Login and Registration Done

--- doctor-portal-client/src/Pages/LogIn/LogIn.js
+++ doctor-portal-client/src/Pages/LogIn/LogIn.js
@@ -1,11 +1,51 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const LogIn = () => {
-    return (
-        <div>
-            <h1>Login</h1>
+  return (
+    <div>
+      <div className="hero min-h-screen bg-base-200" data-theme="corporate">
+        <div className="hero-content flex-col">
+          <div className="text-center lg:text-left mb-5">
+            <h1 className="text-5xl font-bold">Login now!</h1>
+          </div>
+          <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+            <div className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="email"
+                  className="input input-bordered"
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="password"
+                  className="input input-bordered"
+                />
+                <label className="label mt-2">
+                    <p className="label-text-alt">Not Registered Yet? Go for &nbsp;
+                  <Link to="/registration" className="label-text-alt link link-hover">
+                    Registration
+                  </Link></p>
+                </label>
+              </div>
+              <div className="form-control mt-3">
+                <button className="btn btn-primary">Login</button>
+              </div>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default LogIn;
--- doctor-portal-client/src/Pages/Registration/Registration.js
+++ doctor-portal-client/src/Pages/Registration/Registration.js
@@ -1,11 +1,52 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Registration = () => {
-    return (
-        <div>
-            <h1>Registration</h1>
+  return (
+    <div>
+      <div className="hero min-h-screen bg-base-200" data-theme="corporate">
+        <div className="hero-content flex-col">
+          <div className="text-center lg:text-left mb-5">
+            <h1 className="text-5xl font-bold">Registration</h1>
+          </div>
+          <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+            <div className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="email"
+                  className="input input-bordered"
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="password"
+                  className="input input-bordered"
+                />
+                <label className="label text-center mt-2">
+                  <p className="label-text-alt">Goto the &nbsp; 
+                  <Link
+                    to="/login"
+                    className="label-text-alt link link-hover">Login
+                  </Link></p>
+                </label>
+              </div>
+              <div className="form-control mt-3">
+                <button className="btn btn-primary">Registration</button>
+              </div>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Registration;
