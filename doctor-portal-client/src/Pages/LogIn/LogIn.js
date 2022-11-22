838de245ff910446d09c887a8ad3b2073c6ec6fd
Ruhul Amin Parvez <ruhul.parvez@oisbd.net>
2022-11-22 12:39:48 +0600

    firebase initial setup :octocat:

--- doctor-portal-client/src/Pages/LogIn/LogIn.js
+++ doctor-portal-client/src/Pages/LogIn/LogIn.js
@@ -1,7 +1,14 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { Form, Link } from "react-router-dom";
 
 const LogIn = () => {
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  }
+
   return (
     <div>
       <div className="hero min-h-screen bg-base-200">
@@ -9,7 +16,7 @@ const LogIn = () => {
           <div className="text-center lg:text-left mb-5">
             <h1 className="text-5xl font-bold">Login now!</h1>
           </div>
-          <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+          <Form onSubmit={handleSubmit(onSubmit)} className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
             <div className="card-body">
               <div className="form-control">
                 <label className="label">
@@ -17,7 +24,8 @@ const LogIn = () => {
                 </label>
                 <input
                   type="text"
-                  placeholder="email"
+                  {...register("email")}
+                  placeholder="Enter your email"
                   className="input input-bordered"
                   required
                 />
@@ -28,22 +36,34 @@ const LogIn = () => {
                 </label>
                 <input
                   type="password"
-                  placeholder="password"
+                  {...register("password")}
+                  placeholder="Enter your password"
                   className="input input-bordered"
                   required
                 />
-                <label className="label text-center mt-2">
-                    <p className="label-text-alt">Not Registered Yet? Go for &nbsp;
-                  <Link to="/registration" className="label-text-alt link link-hover">
-                    Registration
+                <label className="label text-center">
+                  <Link to="/forget-password" className="label-text-alt link link-hover">
+                  <p className="label-text-alt">Forgot Password?</p>
+                  </Link>
+                </label>
+              </div>
+              <div className="form-control">
+                <button className="btn btn-primary">Login</button>
+              </div>
+              <div>
+                <label className="label text-center">
+                  <p className="label-text-alt">New to Doctor's Portal? &nbsp;
+                  <Link to="/registration" className="label-text-alt link link-hover text-primary">
+                    Create New Account
                   </Link></p>
                 </label>
               </div>
-              <div className="form-control mt-3">
-                <button className="btn btn-primary">Login</button>
+              <div className="divider">OR</div>
+              <div className="form-control">
+                <button className="btn btn-outline btn-primary">Login with Google</button>
               </div>
             </div>
-          </div>
+          </Form>
         </div>
       </div>
     </div>
--- doctor-portal-client/src/Pages/Registration/Registration.js
+++ doctor-portal-client/src/Pages/Registration/Registration.js
@@ -1,23 +1,42 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { Form, Link } from "react-router-dom";
 
 const Registration = () => {
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  }
+
   return (
-    <div>
-      <div className="hero min-h-screen bg-base-200">
+      <div className="hero min-h-screen py-10 bg-base-200">
         <div className="hero-content flex-col">
           <div className="text-center lg:text-left mb-5">
-            <h1 className="text-5xl font-bold">Registration</h1>
+            <h1 className="text-5xl font-bold">Registration now!</h1>
           </div>
-          <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+          <Form onSubmit={handleSubmit(onSubmit)} className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
             <div className="card-body">
+            <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Name</span>
+                </label>
+                <input
+                  type="text"
+                  {...register("name")}
+                  placeholder="Enter your name"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Email</span>
                 </label>
                 <input
                   type="text"
-                  placeholder="email"
+                  {...register("email")}
+                  placeholder="Enter your email"
                   className="input input-bordered"
                   required
                 />
@@ -28,26 +47,31 @@ const Registration = () => {
                 </label>
                 <input
                   type="password"
-                  placeholder="password"
+                  {...register("password")}
+                  placeholder="Enter your password"
                   className="input input-bordered"
                   required
                 />
-                <label className="label text-center mt-2">
-                  <p className="label-text-alt">Goto the &nbsp; 
-                  <Link
-                    to="/login"
-                    className="label-text-alt link link-hover">Login
-                  </Link></p>
-                </label>
               </div>
               <div className="form-control mt-3">
                 <button className="btn btn-primary">Registration</button>
               </div>
+              <div>
+                <label className="label text-center">
+                  <p className="label-text-alt">Already have an account? &nbsp;
+                  <Link to="/login" className="label-text-alt link link-hover text-primary">
+                    Goto Login
+                  </Link></p>
+                </label>
+              </div>
+              <div className="divider">OR</div>
+              <div className="form-control">
+                <button className="btn btn-outline btn-primary">Login with Google</button>
+              </div>
             </div>
-          </div>
+          </Form>
         </div>
       </div>
-    </div>
   );
 };
 
